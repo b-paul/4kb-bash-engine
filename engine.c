3ad248ef9d04fe687dbf6dfcd33f10a48b08af93
GuyClicking <bpaul848@gmail.com>
2020-09-05 19:35:12 +1000

    Name executable the PID of the script to avoid crashes if multiple instances are created

--- engine.c
+++ engine.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
+int board[128];
+
 int main() {
-    char str[20480];
+    char str[2048];
     while (fgets(str, 2048, stdin) == NULL) {
         if (strstr(str, "uci") == str)
             printf("uciok");
